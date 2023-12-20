e4e7da38a6406c144d62a795dbb14493ad931a09
Orla <G00411336@atu.ie>
2023-12-20 20:27:13 +0000

    real server dbconfigpa

---
+++ dbconfigpa.py
@@ -0,0 +1,6 @@
+config = {
+    "hostname":"orlacorry1.mysql.pythonanywhere-services.com", 
+    "username":"orlacorry1",
+    "password":"datareppass", 
+    "databasename":"orlacorry1$datarepresentation"
+}
