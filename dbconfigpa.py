c54372eb65414d5897db7306bd93047e7422c0a2
Orla <G00411336@atu.ie>
2023-12-20 20:48:41 +0000

    Update .gitignore and untrack dbconfigpa.py

--- dbconfigpa.py
+++
@@ -1,6 +0,0 @@
-config = {
-    "hostname":"orlacorry1.mysql.pythonanywhere-services.com", 
-    "username":"orlacorry1",
-    "password":"datareppass", 
-    "databasename":"orlacorry1$datarepresentation"
-}
